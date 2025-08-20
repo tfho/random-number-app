60e5e2860db894e7bd3bf5e116b44e211571f86e
tfho <tfangagnanh@gmail.com>
2025-08-20 21:12:34 +0000

    Suppression de console.log de script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-console.log("Hello")
-
 const randomButton = document.querySelector("#random-btn")
 
 randomButton.addEventListener(
     "click",
     ()=>{
-        document.querySelector("#result").textContent = Math.random()
+        document.querySelector("#result").textContent = Math.floor(
+            getRandomNumber(1,100)
+        )
     }
 )
+
+function getRandomNumber(min, max){
+    return Math.random() * (max - min) + min;
+}
